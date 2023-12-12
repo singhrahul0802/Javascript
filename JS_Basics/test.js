741217218aa1f19c9ef08d00710e8776f3b98abc
singhrahul0802 <102241033+singhrahul0802@users.noreply.github.com>
2023-12-12 07:17:40 +0000

    Basics

--- JS_Basics/test.js
+++ JS_Basics/test.js
@@ -1,1 +1,19 @@
-console.log("Hello Js")
+
+// variables
+const MyNumber = 8839649871; // const keyword cannot be modified later as it is fix. This is used to declare constants
+
+let myname = "Rahul singh"; // mostly used, it is block scoped and can be updated within it's scope. But we can't redeclare it
+
+var city = "Indore"; // global scoped and can be re-declared
+
+Birthplace = "Aligarh"; // we can also declare a variable like this but it's not good practise
+
+let statename; // we can also declare an empty variable and it can be updated later within it's scope.OUTPUT WILL BE UNDEFINED
+
+// MyNumber=56351354; // Not Allowed in JS
+
+myname = "karishma"
+city="bengaluru"
+Birthplace ="indore"
+
+console.table([myname,city,Birthplace,statename]); // this will show the multiple outputs at once in tabular form
